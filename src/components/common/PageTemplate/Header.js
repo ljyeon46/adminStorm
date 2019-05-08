98a4bd2ce4d950bb1450a443d1acb50fd9904a29
JY <ji0096300@naver.com>
2019-05-08 18:00:34 +0900

    2222

--- src/components/common/PageTemplate/Header.js
+++ src/components/common/PageTemplate/Header.js
@@ -1,12 +1,14 @@
 import logo from '../../../styles/img/storm-logo-notext.png';
 import React,{ Component } from 'react';
 import './Header.css';
+import Clock from 'react-live-clock';
 
 class Header extends Component {
     render() {
       return (
           <div className="title-bar">
               <img src={logo} className="title-bar logo" alt="logo"/>
+              <Clock className="clock" format={'YYYY년 MM월 DD일 HH:mm:ss'} ticking={true} timezone={'KR'} />
           </div>
       );
     }
